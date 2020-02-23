fc1810298ff8071b02f6af85a82d4b681e5a2365
Laurynn <48033952+laurynndiby@users.noreply.github.com>
2020-02-23 10:22:46 -0800

    Update uplift.py

--- uplift.py
+++ uplift.py
@@ -6,9 +6,31 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///uplift.db'
 db = SQLAlchemy(app)
 
-@app.route('/')
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    school = db.Column(db.String(50), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<User %r>' % self.id
+
+@app.route('/', methods=['POST','GET'])
 def home():
-    return render_template('home.html')
+    if request.method == 'POST':
+        name1 = request.form['name']
+        new_name = Users(name=name1)
+        school1 = request.form['school']
+        new_school = Users(school=school1)
+        try:
+            db.session.add(new_name)
+            db.session.add(new_school)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue adding your information'
+    else:
+        return render_template('home.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
